99b8f553e53a07efd55d7d93bcc1c85c961b1599
keytrain <keytrain@users.noreply.github.com>
2018-08-30 19:22:13 -0700

    Add ThoughtService init

--- src/services/ThoughtService.js
+++ src/services/ThoughtService.js
@@ -1,12 +1,29 @@
 // import { API } from "aws-amplify"
 const storage = window.localStorage;
 
-export const createThought = parentId => {
-  storage.setItem("parentId");
+export const createThought = (parentId, text) => {
+  let thought = {
+    children: [
+      {
+        id: "thought-uuid",
+        content: {
+          children: "",
+          text: text,
+          metadata: {
+            timestamp: Date.now(),
+          },
+        },
+      },
+    ],
+  };
+
+  storage.setItem(parentId, JSON.stringify(thought));
   // return API.post("users", "/users", {}).catch(e => {
   //   console.warn(e.message)
   // })
 };
+
+export const addThought = id => {};
 
 export const getThought = id => {
   //   return API.get("users", "/users", {}).catch(e => {
